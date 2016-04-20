d523ae6dfc6a93728e9b6914fbe0e596463707c8
gnfisher <me@gnfisher.com>
2016-04-20 12:54:07 -0300

    adding more obvious and verbose tests because i can do that

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
+# A note
+# These tests are very verbose and no doubt needless as mostly
+# they confirm ActiveRecord is doing what it should - of course
+# I am really just confirming that I am USING ActiveRecord as
+# it should be used and practicing my spec-fu.
+
 describe User do
 
   before do
@@ -12,5 +18,15 @@ describe User do
     expect(@user.errors[:email].size).to eq(1) 
   end
   
-  it "is invalid without a username"
+  it "is invalid without a username" do
+    @user.username.clear
+    @user.valid?
+    expect(@user.errors[:username].size).to eq(1)  
+  end
+
+  it "is invalid with incorrect password confirmation" do
+    @user.password_confirmation = "notmypassword"
+    @user.valid?
+    expect(@user.errors[:password_confirmation].size).to eq(1)
+  end
 end
