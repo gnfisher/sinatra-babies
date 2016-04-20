00a89ec5e8d935f5ef0feeccb12b9a40bcafddc8
gnfisher <me@gnfisher.com>
2016-04-20 11:14:51 -0300

    setting up rspec

--- db/migrate/20160419203349_example.rb
+++
@@ -1,7 +0,0 @@
-class Example < ActiveRecord::Migration
-  def change
-    create_table :examle do |t|
-      t.string :name
-    end
-  end
-end
---
+++ spec/models/sample_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'Sample' do
+  it "should run" do
+    true
+  end
+
+  it "should connect to db" do
+  end
+end
+
