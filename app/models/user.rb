6be0b3d134871966ec2ab79a484dfd3e45573f0d
gnfisher <me@gnfisher.com>
2016-04-20 12:39:38 -0300

    first specs passing and communicating with model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 module SinatraBabies
   module Models
     class User < ActiveRecord::Base
+      validates_presence_of :username, :email
+
+      has_secure_password
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  it "is invalid without an email"
+
+  before do
+    @user = User.new(email: "test@test.com", username: "tester", password: "password")
+  end
+
+  it "is invalid without an email" do
+    @user.email.clear
+    @user.valid?
+    expect(@user.errors[:email].size).to eq(1) 
+  end
+  
   it "is invalid without a username"
 end
