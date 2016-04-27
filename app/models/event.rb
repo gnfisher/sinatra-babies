930f609b417f1c9e6089ab781133b989a67d2661
gnfisher <me@gnfisher.com>
2016-04-27 16:59:49 -0300

    writing the hours_slept method, still messy but passes

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,51 @@ module SinatraBabies
 
 
       def self.days_ago(days)
-        Event.where('DATE(time) = ?', (Date.today - days)).order('time desc')
+        self.where('DATE(time) = ?', (Date.today - days)).order('time desc')
+      end
+
+      def self.hours_slept
+        sleep_and_wakes = self.where(event_type_id: 1..2).map do |event|
+          { event: (event.event_type_id == 1 ? "woke" : "slept"), time:  event.time }
+        end.reverse!
+
+        puts sleep_and_wakes
+        puts
+        
+        day = sleep_and_wakes.first[:time].day
+        month = sleep_and_wakes.first[:time].month
+        year = sleep_and_wakes.first[:time].year
+
+        midnight       = Time.new(year, month, day, 0,0,0, "+00:00") 
+        end_of_day     = Time.new(year, month, day + 1, 0,0,0, "+00:00") 
+        sleep_count    = 0
+        still_sleeping = sleep_and_wakes.last[:event] == "slept" ? true : false
+        
+        if still_sleeping
+          sleep_count += end_of_day - sleep_and_wakes.last[:time]
+        end
+          
+        #binding.pry
+
+        sleep_and_wakes.reduce(midnight) do |memo, hash|
+          if hash[:event] == "slept"
+            memo != nil ? memo : memo = hash[:time]
+          else
+            if memo != nil
+              minutes      = hash[:time] - memo
+              sleep_count += minutes
+              memo         = nil
+            else
+              memo
+            end
+          end
+        end
+                
+        hours_slept = {
+          hours:          "#{sleep_count.to_i / 60 / 60}",
+          minutes:        "#{sleep_count.to_i / 60 % 60}",
+          still_sleeping: still_sleeping
+        }
       end
     end
   end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 describe Event do
 
   before do
-    @todays_event = Event.create(baby_id: 1, event_type_id: 1, time: Time.now)
-    @yesterdays_event = Event.create(baby_id: 1, event_type_id: 1, time: Time.now - (24*60*60))
-    @two_days_ago_event = Event.create(baby_id: 1, event_type_id: 1, time: Time.now - (48*60*60))
+    @baby2 = Baby.create(name: "Baby Two")
+    @todays_event = Event.create(baby_id: 1, event_type_id: 3, time: Time.now)
+    @yesterdays_event = Event.create(baby_id: 1, event_type_id: 3, time: Time.now - (24*60*60))
+    @two_days_ago_event = Event.create(baby_id: 1, event_type_id: 3, time: Time.now - (48*60*60))
+    @not_your_baby_event = Event.create(baby_id: 2, event_type_id: 3, time: Time.now)
   end
 
   context 'Validations on create or update' do
@@ -17,6 +19,12 @@ describe Event do
         result = Event.days_ago(0)
         expect(result).to include(@todays_event)
         expect(result).not_to include(@yesterdays_event)
+      end
+
+      it 'should not include another babies events' do
+        lucas = Baby.find(1)
+        result = lucas.events.days_ago(0)
+        expect(result).not_to include(@not_your_baby_event)
       end
     end
 
@@ -34,6 +42,19 @@ describe Event do
         expect(result).to include(@two_days_ago_event)
         expect(result).not_to include(@yesterdays_event)
       end
+    end
+  end
+
+  context 'Event.hours_slept' do
+    it 'should accurately count hours slept since midnight' do
+      result1 = Baby.find(1).events.days_ago(0).hours_slept
+      result2 = Baby.find(1).events.days_ago(1).hours_slept
+      puts result1
+      puts result2
+      expect(result1[:hours]).to eq("8")
+      expect(result1[:still_sleeping]).to be false
+      expect(result2[:hours]).to eq("16")
+      expect(result2[:still_sleeping]).to be true
     end
   end
 end
