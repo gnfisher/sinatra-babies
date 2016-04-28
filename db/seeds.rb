e0244861c4f26bd9928f89dab750d9c77af30f88
gnfisher <me@gnfisher.com>
2016-04-28 11:57:31 -0300

    Updated seeds to be dynamically generated based on current day

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,66 +15,71 @@ lucas.save
 
 # Creating a history of events for Lucas.
 
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-27 08:00:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-27 06:50:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-27 06:20:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-27 06:10:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-27 03:00:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 4, time: '2016-04-27 02:50:00 -0300') # Pooped
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-27 02:20:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-27 02:10:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-26 23:40:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-26 23:00:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-26 23:00:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-26 20:00:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-26 19:15:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 4, time: '2016-04-26 18:55:00 -0300') # Pooped
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-26 18:45:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-26 14:45:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-26 14:00:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-26 13:50:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-26 11:30:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-26 11:00:00 -0300') # Fed
+today = Time.now.strftime("%Y-%m-%d")
+yesterday = (Time.now - 86400).strftime("%Y-%m-%d")
+anteayer = (Time.now - (86400*2)).strftime("%Y-%m-%d")
+three_days_ago = (Time.now - (86400*3)).strftime("%Y-%m-%d")
+
+Event.create(baby_id: 1, event_type_id: 1, time: "#{today} 08:00:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{today} 06:50:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{today} 06:20:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{today} 06:10:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{today} 03:00:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 4, time: "#{today} 02:50:00 -0300") # Pooped
+Event.create(baby_id: 1, event_type_id: 3, time: "#{today} 02:20:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{today} 02:10:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{yesterday} 23:40:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{yesterday} 23:00:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{yesterday} 23:00:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{yesterday} 20:00:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{yesterday} 19:15:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 4, time: "#{yesterday} 18:55:00 -0300") # Pooped
+Event.create(baby_id: 1, event_type_id: 1, time: "#{yesterday} 18:45:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{yesterday} 14:45:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{yesterday} 14:00:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{yesterday} 13:50:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{yesterday} 11:30:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{yesterday} 11:00:00 -0300") # Fed
 
 
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-26 08:00:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-26 06:50:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-26 06:20:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-26 06:10:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-26 03:00:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 4, time: '2016-04-26 02:50:00 -0300') # Pooped
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-26 02:20:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-26 02:10:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-25 23:40:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-25 23:00:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-25 23:00:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-25 20:00:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-25 19:15:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 4, time: '2016-04-25 18:55:00 -0300') # Pooped
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-25 18:45:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-25 14:45:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-25 14:00:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-25 13:50:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-25 11:30:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-25 11:00:00 -0300') # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{yesterday} 08:00:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{yesterday} 06:50:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{yesterday} 06:20:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{yesterday} 06:10:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{yesterday} 03:00:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 4, time: "#{yesterday} 02:50:00 -0300") # Pooped
+Event.create(baby_id: 1, event_type_id: 3, time: "#{yesterday} 02:20:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{yesterday} 02:10:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{anteayer} 23:40:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{anteayer} 23:00:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{anteayer} 23:00:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{anteayer} 20:00:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{anteayer} 19:15:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 4, time: "#{anteayer} 18:55:00 -0300") # Pooped
+Event.create(baby_id: 1, event_type_id: 1, time: "#{anteayer} 18:45:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{anteayer} 14:45:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{anteayer} 14:00:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{anteayer} 13:50:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{anteayer} 11:30:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{anteayer} 11:00:00 -0300") # Fed
 
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-25 08:00:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-25 06:50:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-25 06:20:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-25 06:10:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-25 03:00:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 4, time: '2016-04-25 02:50:00 -0300') # Pooped
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-25 02:20:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-25 02:10:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-24 23:40:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-24 23:00:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-24 23:00:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-24 20:00:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-24 19:15:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 4, time: '2016-04-24 18:55:00 -0300') # Pooped
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-24 18:45:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-24 14:45:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-24 14:00:00 -0300') # Fed
-Event.create(baby_id: 1, event_type_id: 1, time: '2016-04-24 13:50:00 -0300') # Woke up
-Event.create(baby_id: 1, event_type_id: 2, time: '2016-04-24 11:30:00 -0300') # Fell asleep
-Event.create(baby_id: 1, event_type_id: 3, time: '2016-04-24 11:00:00 -0300') # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{anteayer} 08:00:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{anteayer} 06:50:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{anteayer} 06:20:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{anteayer} 06:10:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{anteayer} 03:00:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 4, time: "#{anteayer} 02:50:00 -0300") # Pooped
+Event.create(baby_id: 1, event_type_id: 3, time: "#{anteayer} 02:20:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{anteayer} 02:10:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{three_days_ago} 23:40:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{three_days_ago} 23:00:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{three_days_ago} 23:00:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{three_days_ago} 20:00:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{three_days_ago} 19:15:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 4, time: "#{three_days_ago} 18:55:00 -0300") # Pooped
+Event.create(baby_id: 1, event_type_id: 1, time: "#{three_days_ago} 18:45:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{three_days_ago} 14:45:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{three_days_ago} 14:00:00 -0300") # Fed
+Event.create(baby_id: 1, event_type_id: 1, time: "#{three_days_ago} 13:50:00 -0300") # Woke up
+Event.create(baby_id: 1, event_type_id: 2, time: "#{three_days_ago} 11:30:00 -0300") # Fell asleep
+Event.create(baby_id: 1, event_type_id: 3, time: "#{three_days_ago} 11:00:00 -0300") # Fed
